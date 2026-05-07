4626dafe848ed078ed127ca8c47d6afb5f84fbe3
AKIRU <a69881018@gmail.com>
2026-05-07 12:57:27 +0530

    Add src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 "use client";
+import React from "react";
 
 import { useScrollReveal } from "@/hooks/useScrollReveal";
 import ParticleCanvas from "@/components/ParticleCanvas";
